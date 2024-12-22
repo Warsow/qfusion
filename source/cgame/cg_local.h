f9b78346a1c5b40297f660d8f4bf5aaa8d832a53
dnk777 <dnk777@users.noreply.github.com>
2024-12-23 00:53:20 +0300

    WIP

--- source/cgame/cg_local.h
+++ source/cgame/cg_local.h
@@ -478,16 +478,9 @@ typedef struct {
 
 	int fullclipShaderNum;
 
-	// fonts
-	char fontSystemFamily[MAX_QPATH];
-	char fontSystemMonoFamily[MAX_QPATH];
-	int fontSystemSmallSize;
-	int fontSystemMediumSize;
-	int fontSystemBigSize;
-
-	struct qfontface_s *fontSystemSmall;
-	struct qfontface_s *fontSystemMedium;
-	struct qfontface_s *fontSystemBig;
+	struct qfontface_s *font2DTiny;
+	struct qfontface_s *font2DMedium;
+	struct qfontface_s *font2DBig;
 
 	MediaCache media;
 
